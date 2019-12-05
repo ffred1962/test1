cec9ef4d5c77e9b5e9a107441e2b2a7c1e9e880f
ffred1962 <fred@mmg.dp.ua>
2019-12-05 12:19:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
-/*
-* main.tf
-*/
+provider "aws" {
+	region = "us-east-1"
+}
+
+resource "aws_instance" "my-instance" {
+	ami = "ami-04169656fea786776"
+	instance_type = "t2.nano"
+	tags = {
+		Name = "Terraform"	
+		Batch = "5AM"
+	}
+}
