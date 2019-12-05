690d9eeebc7d5a60abd2a24a0b383f3f18ca5865
ffred1962 <fred@mmg.dp.ua>
2019-12-05 12:43:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
-provider "aws" {
-	region = "us-east-1"
+resource "aws_key_pair" "terraform-demo" {
+  key_name   = "terraform-demo"
+  public_key = "${file("terraform-demo.pub")}"
 }
 
 resource "aws_instance" "my-instance" {
-	ami = "ami-04169656fea786776"
-	instance_type = "t2.nano"
-	tags = {
-		Name = "Terraform"	
-		Batch = "5AM"
-	}
+  count         = "${var.instance_count}"
+  ami           = "${lookup(var.ami,var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${aws_key_pair.terraform-demo.key_name}"
+  user_data     = "${file("install_apache.sh")}"
+
+  tags = {
+    Name  = "${element(var.instance_tags, count.index)}"
+    Batch = "5AM"
+  }
 }
